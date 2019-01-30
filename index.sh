c7db680beccf08e02c877f63cfd1d70d0170a1b1
sander shan <shanzy@ydd100.cn>
2019-01-30 09:44:54 +0800

    添加了脚本

--- index.sh
+++ index.sh
@@ -1,24 +1,73 @@
 
-
-# npm install react-navigation --save
-
-# npm install react-redux --save
-
-# npm install redux --save
-
-# npm install redux-saga --save
-
-# npm install redux-persist --save
-
-# npm install reduxsauce --save
-
-# npm install prop-types --save
-
-# npm install react-native-root-toast --save
-
-# npm install react-native-vector-icons --save
+#!/usr/bin/env bash
 
 
-# npm install react-native-device-info --save
 
-# npm install ramda --save
+
+
+basedir=`cd $(dirname $0); pwd -P`
+
+echo "当前的路径 = " + $basedir;
+
+
+echo "请输入项目的路径 : "
+
+read root_path
+
+echo "输入的项目路径 " + $root_path;
+
+
+
+
+if [ ! -d "$root_path" ]; then
+	mkdir $root_path
+fi
+
+
+echo "请输入项目的名称 " 
+read root_name
+
+cd $root_path
+
+react-native init $root_name
+
+
+
+cp -R "$basedir/app" "$root_path/$root_name"
+
+
+cd "$root_path/$root_name"
+
+npm install react-navigation --save
+
+npm install react-redux --save
+
+npm install redux --save
+
+npm install redux-saga --save
+
+npm install redux-persist --save
+
+npm install reduxsauce --save
+
+npm install prop-types --save
+
+npm install react-native-root-toast --save
+
+npm install react-native-vector-icons --save
+
+npm install react-native-device-info --save
+
+npm install ramda --save
+
+npm install react-native-gesture-handler --save
+
+
+react-native link
+
+
+
+
+
+
+
